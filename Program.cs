8f4ead251607a675d68b797691dbd2aaabed5881
Horațiu Mlendea <Horatiu.Mlendea@ProtonMail.com>
2022-12-17 15:06:32 +0200

    Implementation

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SG = SteamGuard.TOTP.SteamGuard;
+
+namespace SteamTOTP
+{
+    public sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            SG steamGuard = new SG();
+            string code = steamGuard.GenerateAuthenticationCode(args[0]);
+            Console.WriteLine(code);
+        }
+    }
+}
