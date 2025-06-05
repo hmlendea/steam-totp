ad0d241208ecb48e233e1b66e57948649d3090d8
Horațiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2025-06-05 17:09:12 +0300

    refactoring: Simplified the code

--- Program.cs
+++ Program.cs
@@ -5,10 +5,6 @@ namespace SteamTOTP
     public sealed class Program
     {
         public static void Main(string[] args)
-        {
-            SG steamGuard = new SG();
-            string code = steamGuard.GenerateAuthenticationCode(args[0]);
-            Console.WriteLine(code);
-        }
+            => Console.WriteLine(new SG().GenerateAuthenticationCode(args[0]));
     }
 }
